e2915b329bc686bda8ccdbd151f46a53d83a90f2
nguyenngan <ngannt1710@gmail.com>
2016-05-19 22:21:37 +0000

    tdd-cycle finished

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
+  
   resources :movies
   match '/movies/:id/similar(.:format)', to: 'movies#similar', as: 'movie_similar', via: :get
   root :to => redirect('/movies')
+  
 end
